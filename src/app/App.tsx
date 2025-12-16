f7b59252c493f4ba750d2a30027d4854fa3331c3
gcoogan1 <gcoogan1@gmail.com>
2025-12-16 11:41:25 -0500

    chore: configure/setup supabase, folders, paths, and redirects(* netlify)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 
 
 const  App = () => {
+
   return (
     <>
       <h1>MotorSport Leagues</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@components': path.resolve(__dirname, './src/app/components'),
+      '@features': path.resolve(__dirname, './src/app/features'),
+      '@pages': path.resolve(__dirname, './src/app/pages'),
+      '@store': path.resolve(__dirname, './src/app/store'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/app/hooks'),
+      '@services': path.resolve(__dirname, './src/app/services'),
+    },
+  },
 })
