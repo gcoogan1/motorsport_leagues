07763b86254a796e12e9aac2ee6602389b6f9077
gcoogan1 <gcoogan1@gmail.com>
2025-12-17 14:35:30 -0500

    style: create color,theme, and typography design tokens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,33 @@
+import styled from "styled-components";
+import { colorTokens } from "./design/tokens/color";
+import { fontCSS, typographyTokens } from "./design/tokens/typography";
 
+const backgroundColor = colorTokens.colors.base.base1;
 
-const  App = () => {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 16px;
+  width: 100vw;
+  background: ${backgroundColor};
+`;
+
+const SubText = styled.p`
+  ${fontCSS(typographyTokens.typography.body.smallRegular)};
+  background: transparent;
+`;
+
+const App = () => {
 
   return (
-    <>
-      <h1>MotorSport Leagues</h1>
-    </>
-  )
-}
+    <Container>
+      <h1 style={{ background: "transparent"}}>Welcome to the Motorsport Leagues App!</h1>
+      <SubText>Background Color: {backgroundColor}</SubText>
+    </Container>
+  );
+};
 
-export default App
+export default App;
---
+++ src/app/design/tokens/color.ts
@@ -0,0 +1,118 @@
+// --- COLOR MODULE --- //
+
+// Types
+type Direction = "left" | "right" | "top" | "bottom";
+type Opacity = 0 | 10 | 20 | 30 | 100;
+type ColorTokens = {
+  colors: {
+    base: Record<string, string>;
+    text: Record<string, string>;
+    role: Record<string, string>;
+    position: Record<string, string>;
+    utility: Record<string, string>;
+  };
+  gradients: {
+    base: Record<string, string>;
+    position: Record<string, string>;
+  };
+};
+
+// -- Opacity -- //
+// Helper
+export const opacity: Record<Opacity, number> = {
+  0: 0.0,
+  10: 0.1,
+  20: 0.2,
+  30: 0.3,
+  100: 1.0,
+};
+// Function
+// Convert hex to rgba with opacity
+export const withOpacity = (hex: string, opacityLevel: Opacity) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  // converted opacity
+  const o = opacity[opacityLevel];
+
+  return `rgba(${r}, ${g}, ${b}, ${o})`;
+};
+
+// -- Gradients -- //
+// Helper
+const gradientDirection: Record<Direction, number> = {
+  left: 270,
+  top: 0,
+  right: 90,
+  bottom: 180,
+};
+// Function
+// Create a fade gradient
+export const fadeGradient = (
+  color: string,
+  opacityLevel: Opacity,
+  direction: Direction,
+  changePercent?: 30 | 50
+) => {
+  const percentage = changePercent ? `${changePercent}%` : "100%";
+  return `linear-gradient(
+    ${gradientDirection[direction]}deg,
+    ${withOpacity(color, opacityLevel)} 0%,
+    ${withOpacity(color, 0)} ${percentage}
+  )`;
+};
+
+// --- COLOR TOKENS --- //
+export const colorTokens: ColorTokens = {
+  colors: {
+    base: {
+      base1: "#080808",
+      base2: "#151515",
+      base3: "#2D2D2D",
+      base4: "#4B4B4B",
+      translucent10: withOpacity("#FFFFFF", 10),
+      translucent20: withOpacity("#FFFFFF", 20),
+      translucent30: withOpacity("#FFFFFF", 30),
+    },
+    text: {
+      text1: "#FFFFFF",
+      text2: "#A5A5A5",
+      text3: "#858585",
+    },
+    role: {
+      director: "#FF5900",
+      champion: "#8C00FF",
+    },
+    position: {
+      gold: "#D0BA03",
+      silver: "#7A7675",
+      bronze: "#3C2617",
+    },
+    utility: {
+      focus: "#0044FF",
+      link: "#00C4FF",
+      alert: "#FF0000",
+    },
+  },
+  gradients: {
+    base: {
+      fadeLeft10: fadeGradient("#FFFFFF", 10, "left"),
+      fadeTop10: fadeGradient("#FFFFFF", 10, "top"),
+      fadeRight10: fadeGradient("#FFFFFF", 10, "right"),
+      fadeBottom10: fadeGradient("#FFFFFF", 10, "bottom"),
+      fadeLeft20: fadeGradient("#FFFFFF", 20, "left"),
+      fadeTop20: fadeGradient("#FFFFFF", 20, "top"),
+      fadeRight20: fadeGradient("#FFFFFF", 20, "right"),
+      fadeBottom20: fadeGradient("#FFFFFF", 20, "bottom"),
+      fadeOutHorizontal10:
+        "linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0) 100%)",
+      fadeOutHorizontal20:
+        "linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 50%, rgba(255, 255, 255, 0) 100%)",
+    },
+    position: {
+      goldLeft: fadeGradient("#D0BA03", 100, "left"),
+      silverLeft: fadeGradient("#7A7675", 100, "left"),
+      bronzeLeft: fadeGradient("#3C2617", 100, "left"),
+    },
+  },
+};
