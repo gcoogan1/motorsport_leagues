6341f2f6df82f16022a69e197c1cf94afae1312b
gcoogan1 <gcoogan1@gmail.com>
2025-12-19 17:23:19 -0500

    feat: create layout and footer app components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,63 +1,7 @@
-import styled from "styled-components";
-import { designTokens } from "./design/tokens/index";
-
-import Chat from "@assets/Icon/Chat.svg?react";
-import Avatar from "@assets/Avatar/Avatar.png";
-import Badge3 from "@assets/SquadBadge/Badge3.png";
-import MLLogo from "@assets/Logos/MS/MS_Vertical.svg?react";
-import Ace from "@assets/Logos/Games/ACEvo.png"
-
-const { colors, typography, borders, effects, layout } = designTokens;
-const backgroundColor = colors.base.base1;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  /* background: ${backgroundColor}; */
-`;
-
-const Content = styled.div`
-  /* ${effects.opacity.opacity50}; */
-  ${effects.boxShadow.coverBaseDown}
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 16px;
-  color: ${colors.text.text1};
-  padding: ${layout.space.large};
-  /* border-color: ${colors.base.base3}; */
-  /* border-width: ${borders.width.medium};
-  border-style: solid; */
-  background-color: #000000;
-  /* background: url(${Badge3}) no-repeat center center; */
-  border-radius: ${borders.radius.large};
-
-`;
-
-const SubText = styled.p`
-  ${typography.subtitle.smallItalic};
-  background: transparent;
-`;
+import Layout from "./components/layout/layout";
 
 const App = () => {
-
-  return (
-    <Container>
-      <Content>
-        <h1 style={{ background: "transparent"}}>Welcome to the Motorsport Leagues App!</h1>
-        <SubText>Background Color: {backgroundColor}</SubText>
-        <Chat width={48} height={48} color={colors.role.director} />
-        <MLLogo color={colors.position.gold} />
-        <img src={Avatar} alt="Avatar" width={150} />
-        <img src={Ace} alt="iRacing Logo" width={150} />
-      </Content>
-    </Container>
-  );
+  return <Layout />;
 };
 
 export default App;
