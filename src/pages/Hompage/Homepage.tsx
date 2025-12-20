bab1417a743340d4a0500092be2b401182ded513
gcoogan1 <gcoogan1@gmail.com>
2025-12-20 18:21:43 -0500

    feat: create homepage with layout and routing

---
+++ src/pages/Hompage/Homepage.tsx
@@ -0,0 +1,14 @@
+import { Container, Logo, SubTitle, Wrapper } from "./Homepage.styles"
+
+const Homepage = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <Logo />
+        <SubTitle>Coming Soon</SubTitle>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export default Homepage
