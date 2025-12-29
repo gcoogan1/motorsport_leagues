c1979457febc9342d51618a08ffe331b27933e49
gcoogan1 <gcoogan1@gmail.com>
2025-12-28 22:11:57 -0500

    feat: create core, user, and guest navbars

---
+++ src/app/components/navbar/variants/core/CoreNavbar.tsx
@@ -0,0 +1,57 @@
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import Button from "@/components/Button/Button";
+import Back from "@assets/Icon/Arrow_Backward.svg?react";
+import NavLayout from "../../layout/NavLayout";
+import NavBrand from "../../components/navBrand/NavBrand";
+import NavAccount from "../../components/navAccount/NavAccount";
+import NavNotification from "../../components/navNotification/NavNotification";
+import {
+  CenterContainer,
+  LeftContainer,
+  RightContainer,
+} from "./CoreNavbar.styles";
+
+type CoreNavbarProps = {
+  countNotifications?: number;
+  accountLabel?: string;
+};
+
+const CoreNavbar = ({ countNotifications, accountLabel }: CoreNavbarProps) => {
+  const isMobile = useMediaQuery("(max-width: 919px)");
+
+  return (
+    <NavLayout>
+      <LeftContainer>
+        {isMobile ? (
+          <Button
+            color="base"
+            rounded
+            iconOnly
+            icon={{ left: <Back /> }}
+            onClick={() => {}}
+          />
+        ) : (
+          <Button
+            color="base"
+            rounded
+            icon={{ left: <Back /> }}
+            onClick={() => {}}
+          >
+            Back
+          </Button>
+        )}
+      </LeftContainer>
+      <CenterContainer>
+        <NavBrand />
+      </CenterContainer>
+      <RightContainer>
+        <>
+          <NavNotification count={countNotifications} />
+          <NavAccount label={accountLabel} />
+        </>
+      </RightContainer>
+    </NavLayout>
+  );
+};
+
+export default CoreNavbar;
---
+++ src/app/components/navbar/variants/guest/GuestNavbar.tsx
@@ -0,0 +1,42 @@
+import Button from "@/components/Button/Button";
+import Profile from "@assets/Icon/Profile.svg?react";
+import Squad from "@assets/Icon/Squad.svg?react";
+import League from "@assets/Icon/League.svg?react";
+import NavBrand from "../../components/navBrand/NavBrand";
+import NavSelect from "../../components/navSelect/NavSelect";
+import NavLayout from "../../layout/NavLayout";
+import {
+  CenterContainer,
+  LeftContainer,
+  RightContainer,
+} from "./GuestNavbar.styles";
+
+
+const GuestNavbar = () => {
+  return (
+    <NavLayout>
+      <LeftContainer>
+        <NavBrand />
+      </LeftContainer>
+      <CenterContainer>
+        <>
+          <NavSelect icon={<Profile />} label={"Profiles"} />
+          <NavSelect icon={<Squad />} label={"Squads"} />
+          <NavSelect icon={<League />} label={"Leagues"} />
+        </>
+      </CenterContainer>
+      <RightContainer>
+        <>
+          <Button color="system" variant="outlined" onClick={() => {}}>
+            Log In
+          </Button>
+          <Button color="system" onClick={() => {}}>
+            Get Started
+          </Button>
+        </>
+      </RightContainer>
+    </NavLayout>
+  );
+};
+
+export default GuestNavbar;
