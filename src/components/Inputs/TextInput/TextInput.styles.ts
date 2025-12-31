4269da713607d934444b1151cfffd45eb3247dae
gcoogan1 <gcoogan1@gmail.com>
2025-12-31 15:09:31 -0500

    feat: create text input and story

---
+++ src/components/Inputs/TextInput/TextInput.styles.ts
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+import { designTokens } from "@/app/design/tokens";
+
+const { colors, layout, typography, borders } = designTokens;
+
+
+export const IconWrapper = styled.span<{ $hasValue?: boolean }>`
+  position: absolute;
+  left: ${layout.space.xSmall};
+  margin-top: ${layout.space.small};
+  pointer-events: none;
+  display: flex;
+  color: ${({ $hasValue }) => ($hasValue ? colors.text.text1 : colors.text.text2)};
+  width: 20px;
+  height: 20px;
+`;
+
+
+export const InputWrapper = styled.div<{ $hasValue?: boolean }>`
+  width: 320px;
+  position: relative;
+  /* When the input is focused, change the icon color */
+  &:focus-within ${IconWrapper} {
+    color: ${colors.text.text1};
+  }
+  /* When the input is active, change the icon color */
+  &:active ${IconWrapper} {
+    color: ${({ $hasValue }) => ($hasValue ? colors.text.text1 : colors.text.text2)};
+  }
+`;
+
+export const LabelRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: ${layout.space.xxxSmall};
+`;
+
+export const Label = styled.label`
+  ${typography.body.smallBold}
+  color: ${colors.text.text2};
+`;
+
+export const Count = styled.span`
+  ${typography.body.tinyRegular}
+  color: ${colors.text.text3};
+`;
+
+export const InputField = styled.input<{ $hasError: boolean; $hasValue: boolean }>`
+  width: 100%;
+  border-radius: ${borders.radius.medium};
+  padding-top: ${layout.space.medium};
+  padding-bottom: ${layout.space.medium};
+  padding-left: 36px;
+  padding-right: ${layout.space.medium};
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: ${layout.space.xSmall};
+  background: ${colors.base.translucent10};
+  color: ${({ $hasValue }) => ($hasValue ? colors.text.text1 : colors.text.text2)};
+  box-shadow: ${({ $hasError }) =>
+    $hasError ? `inset 0 0 0 2px ${colors.alert.alertA}` : `inset 0 0 0 2px transparent`};
+
+  &::placeholder {
+    ${typography.body.mediumRegular}
+  }
+
+  &:hover {
+    background: ${colors.base.translucent20};
+  }
+
+  &:focus-visible {
+    background: transparent;
+    outline: 2px solid ${colors.utility.focus};
+    outline-offset: 2px;
+    color: ${colors.text.text1};
+    box-shadow: inset 0 0 0 2px ${colors.text.text1};
+  }
+
+  &:active {
+    box-shadow: inset 0 0 0 2px ${colors.text.text1};
+    background: transparent;
+    outline: none;
+  }
+`;
+
+export const HelperText = styled.span`
+  ${typography.body.tinyRegular}
+  color: ${colors.text.text2};
+  display: flex;
+  margin-top: 3px;
+`;
+
+export const ErrorText = styled.span`
+  ${typography.body.smallBold}
+  color: ${colors.alert.alertA};
+  display: flex;
+  align-items: center;
+  gap: ${layout.space.xxxSmall};
+`;
---
+++ src/components/Inputs/TextInput/TextInput.tsx
@@ -0,0 +1,75 @@
+import { useFormContext } from "react-hook-form";
+import Error_Outlined from "@assets/Icon/Error_Outlined.svg?react";
+import {
+  Count,
+  ErrorText,
+  HelperText,
+  IconWrapper,
+  InputField,
+  InputWrapper,
+  Label,
+  LabelRow,
+} from "./TextInput.styles";
+
+// MUST BE WRAPPED IN REACT-HOOK-FORM FORM PROVIDER
+
+type TextInputProps = {
+  name: string;
+  label: string;
+  icon?: React.ReactNode;
+  placeholder?: string;
+  maxLength?: number;
+  showCounter?: boolean;
+  count?: number;
+  helperText?: string;
+  hasError?: boolean;
+  errorMessage?: string;
+};
+
+const TextInput = ({
+  name,
+  label,
+  icon,
+  placeholder,
+  maxLength = 1000, 
+  showCounter,
+  helperText,
+  hasError,
+  errorMessage,
+}: TextInputProps) => {
+
+  const { register, watch } = useFormContext();
+
+  const value = watch(name);
+  const hasValue = Boolean(value && value.length > 0);
+
+  const count = value ? value.length : 0;
+
+  return (
+    <InputWrapper $hasValue={hasValue}>
+      <LabelRow>
+        <Label>{label}</Label>
+        {!!showCounter && <Count>{count}/999</Count>}
+      </LabelRow>
+      {/* <FieldRow> */}
+      {icon && <IconWrapper $hasValue={!!value}>{icon}</IconWrapper>}
+      <InputField
+        id={name}
+        {...register(name)}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        $hasError={!!hasError}
+        $hasValue={hasValue}
+      />
+      {/* </FieldRow> */}
+      <HelperText>{helperText}</HelperText>
+      {!!hasError && (
+        <ErrorText>
+          <Error_Outlined width={18} height={18} /> {errorMessage}
+        </ErrorText>
+      )}
+    </InputWrapper>
+  );
+};
+
+export default TextInput;
