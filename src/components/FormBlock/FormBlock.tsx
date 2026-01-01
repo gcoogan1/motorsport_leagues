32333d33654bd50012af14ce5b2ef667b07cf58d
gcoogan1 <gcoogan1@gmail.com>
2026-01-01 18:03:37 -0500

    feat: create formblock component and stories

---
+++ src/components/FormBlock/FormBlock.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import {
+  FormWrapper,
+  FormHeader,
+  HeaderTitle,
+  FormBody,
+  BodyHeader,
+  BodyTitle,
+  BodySubHeader,
+  BodyInputContainer,
+  FormActions,
+  SecondaryButtonContainer,
+} from "./FormBlock.styles";
+import Button from "../Button/Button";
+
+type FormBlockProps = {
+  title: string;
+  question: string;
+  helperMessage?: string;
+  children?: React.ReactNode;
+  onContinue?: () => void;
+  onCancel?: () => void;
+};
+
+const FormBlock = ({
+  title,
+  question,
+  helperMessage,
+  children,
+  onContinue,
+  onCancel,
+}: FormBlockProps) => {
+  const isMobile = useMediaQuery("(max-width: 919px)");
+
+  const formMethods = useForm();
+  const { handleSubmit } = formMethods;
+
+  const handleOnContinue = () => {
+    if (onContinue) {
+      onContinue();
+    }
+  };
+
+  const handleOnCancel = () => {
+    if (onCancel) {
+      onCancel();
+    }
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <FormWrapper onSubmit={handleSubmit(handleOnContinue)}>
+        <FormHeader>
+          <HeaderTitle>{title}</HeaderTitle>
+        </FormHeader>
+        <FormBody $isMobile={isMobile}>
+          <BodyHeader>
+            <BodyTitle>{question}</BodyTitle>
+            <BodySubHeader>{helperMessage}</BodySubHeader>
+          </BodyHeader>
+          <BodyInputContainer>{children}</BodyInputContainer>
+        </FormBody>
+        <FormActions>
+          <SecondaryButtonContainer>
+            <Button color="base" variant="ghost" onClick={handleOnCancel}>
+              Cancel
+            </Button>
+          </SecondaryButtonContainer>
+          <Button type="submit" color="system">
+            Continue
+          </Button>
+        </FormActions>
+      </FormWrapper>
+    </FormProvider>
+  );
+};
+
+export default FormBlock;
