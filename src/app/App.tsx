cee9275ae5add7bda94924cf354e68df8b6da552
gcoogan1 <gcoogan1@gmail.com>
2026-01-05 19:02:15 -0500

    feat: create modal provider and wrap app with it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import ReactGA from "react-ga4";
 
 import AppRouter from "./routes/AppRouter";
-import { AppThemeProvider } from "./design/AppThemeProvider";
+import { AppThemeProvider } from "../providers/theme/AppThemeProvider";
+import { ModalProvider } from "../providers/modal/ModalProvider";
 // import Toast from "@/components/Messages/Toast/Toast";
 
 //TODO: Re-enable Toast component once global state management is implemented
@@ -14,9 +15,11 @@ const App = () => {
 
   return (
     <AppThemeProvider>
-      {/* Toast component temporarily disabled */}
-      {/* <Toast usage="success" message="This is an informational message." /> */}
-      <AppRouter />
+      <ModalProvider>
+        {/* Toast component temporarily disabled */}
+        {/* <Toast usage="success" message="This is an informational message." /> */}
+        <AppRouter />
+      </ModalProvider>
     </AppThemeProvider>
   );
 };
