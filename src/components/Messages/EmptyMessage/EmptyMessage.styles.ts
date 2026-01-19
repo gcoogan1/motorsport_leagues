4a2095fc9547491bd1b5accf70cbaaa2c7629dd3
gcoogan1 <gcoogan1@gmail.com>
2026-01-19 12:22:04 -0500

    fix: update empty message component to take full button props

--- src/components/Messages/EmptyMessage/EmptyMessage.styles.ts
+++ src/components/Messages/EmptyMessage/EmptyMessage.styles.ts
@@ -67,8 +67,6 @@ export const ActionsContainer = styled.div`
   })}
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 100%;
   padding-top: ${layout.space.medium};
   gap: ${layout.space.xSmall};
