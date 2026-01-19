765b028859fd9bcef7d147c6dc885d627ec3037f
gcoogan1 <gcoogan1@gmail.com>
2026-01-19 12:22:42 -0500

    fix: update guest panels to use empty message buttons

--- src/features/panels/leagues/GuestLeaguesPanel.tsx
+++ src/features/panels/leagues/GuestLeaguesPanel.tsx
@@ -1,26 +1,9 @@
-import styled from "styled-components";
 import { usePanel } from "@/providers/panel/usePanel";
 import { navigate } from "@/app/navigation/navigation";
-import { designTokens } from "@/app/design/tokens";
-import { topFadeBorder } from "@/app/design/mixens/edgeFadeBorder";
 import EmptyMessage from "@/components/Messages/EmptyMessage/EmptyMessage";
 import PanelLayout from "@/components/Panels/components/PanelLayout/PanelLayout";
 import League from "@assets/Icon/League.svg?react";
-import Button from "@/components/Button/Button";
 
-const { layout, gradients, borders } = designTokens;
-
-const ActionsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  gap: ${layout.space.xSmall};
-  padding-top: ${layout.space.medium};
-  ${topFadeBorder({
-    gradient: gradients.base.fadeOutHorizontal10,
-    width: borders.width.thin,
-  })}
-`;
 
 const GuestLeaguesPanel = () => {
 
@@ -43,11 +26,17 @@ const GuestLeaguesPanel = () => {
         title="Sign Up or Log In"
         icon={<League />}
         subtitle="Participate in Leagues with your driver Profiles and Squads. Get started now by creating an account."
+        actions={{
+          primary: {
+            onClick: handleGoToSignUp,
+            label: "Create Account",
+          },
+          secondary: {
+            onClick: handleGoToLogin,
+            label: "Log In",
+          },
+        }}
       />
-      <ActionsContainer>
-        <Button color="system" onClick={handleGoToSignUp}>Create Account</Button>
-        <Button color="system" variant="outlined" onClick={handleGoToLogin}>Log In</Button>
-      </ActionsContainer>
     </PanelLayout>
   );
 };
--- src/features/panels/profiles/GuestProfilesPanel.tsx
+++ src/features/panels/profiles/GuestProfilesPanel.tsx
@@ -1,26 +1,10 @@
-import styled from "styled-components";
 import { usePanel } from "@/providers/panel/usePanel";
 import { navigate } from "@/app/navigation/navigation";
-import { designTokens } from "@/app/design/tokens";
-import { topFadeBorder } from "@/app/design/mixens/edgeFadeBorder";
 import EmptyMessage from "@/components/Messages/EmptyMessage/EmptyMessage";
 import PanelLayout from "@/components/Panels/components/PanelLayout/PanelLayout";
 import Profile from "@assets/Icon/Profile.svg?react";
-import Button from "@/components/Button/Button";
 
-const { layout, gradients, borders } = designTokens;
 
-const ActionsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  gap: ${layout.space.xSmall};
-  padding-top: ${layout.space.medium};
-  ${topFadeBorder({
-    gradient: gradients.base.fadeOutHorizontal10,
-    width: borders.width.thin,
-  })}
-`;
 
 const GuestProfilesPanel = () => {
 
@@ -43,11 +27,17 @@ const GuestProfilesPanel = () => {
         title="Sign Up or Log In"
         icon={<Profile />}
         subtitle="Participate in Leagues with your driver Profiles and Squads. Get started now by creating an account."
+        actions={{
+          primary: {
+            onClick: handleGoToSignUp,
+            label: "Create Account",
+          },
+          secondary: {
+            onClick: handleGoToLogin,
+            label: "Log In",
+          },
+        }}
       />
-      <ActionsContainer>
-        <Button color="system" onClick={handleGoToSignUp}>Create Account</Button>
-        <Button color="system" variant="outlined" onClick={handleGoToLogin}>Log In</Button>
-      </ActionsContainer>
     </PanelLayout>
   );
 };
