c51d9e98df3bffaa7f086bd6cb2dc546be9f6a44
gcoogan1 <gcoogan1@gmail.com>
2026-01-20 15:02:25 -0500

    fix: remove unused global error modals

--- src/types/modal.types.ts
+++ src/types/modal.types.ts
@@ -3,16 +3,16 @@ import ServerError from "../features/auth/modals/errors/SeverError/ServerError";
 import TooManyAttempts from "../features/auth/modals/errors/AttemptMax/AttemptMax";
 import RequestMax from "../features/auth/modals/errors/RequestMax/RequestMax";
 import AccountVerified from "../features/auth/modals/success/AccountVerifed/AccountVerified";
-import CodeResent from "../features/auth/modals/success/CodeResent/CodeResent";
 import IncorrectCred from "../features/auth/modals/errors/IncorrectCred/IncorrectCred";
-import UnverifiedAccount from "../features/auth/modals/errors/UnverifiedAccount/UnverifiedAccount";
 import AccountSuspended from "../features/auth/modals/errors/AccountSuspended/AccountSuspended";
 import ExistingEmail from "@/features/panels/account/modals/errors/ExistingEmail/ExistingEmail";
 import IncorrectPassword from "@/features/panels/account/modals/errors/IncorrectPassword/IncorrectPassword";
 
 // -- Modal Types & Variants -- //
 
-export type MODAL_TYPES = 'EXISTING_ACCOUNT' | 'SERVER_ERROR' | 'ATTEMPT_MAX' | 'REQUEST_MAX' | 'CODE_RESENT' | 'ACCOUNT_VERIFIED' | 'INCORRECT_CRED' | 'ACCOUNT_UNVERIFIED' | 'ACCOUNT_SUSPENDED' | 'EXISTING_EMAIL' | 'INCORRECT_PASSWORD';
+// Modals that do not require props
+
+export type MODAL_TYPES = 'EXISTING_ACCOUNT' | 'SERVER_ERROR' | 'ATTEMPT_MAX' | 'REQUEST_MAX' | 'ACCOUNT_VERIFIED' | 'INCORRECT_CRED' | 'ACCOUNT_SUSPENDED' | 'EXISTING_EMAIL' | 'INCORRECT_PASSWORD';
 
 export const modalVariants: Record<MODAL_TYPES, React.FC> = {
   EXISTING_ACCOUNT: ExistingAccount,
@@ -21,9 +21,7 @@ export const modalVariants: Record<MODAL_TYPES, React.FC> = {
   ATTEMPT_MAX: TooManyAttempts,
   REQUEST_MAX: RequestMax,
   INCORRECT_CRED: IncorrectCred,
-  CODE_RESENT: CodeResent,
   ACCOUNT_VERIFIED: AccountVerified,
-  ACCOUNT_UNVERIFIED: UnverifiedAccount,
   ACCOUNT_SUSPENDED: AccountSuspended,
   INCORRECT_PASSWORD: IncorrectPassword
 };
