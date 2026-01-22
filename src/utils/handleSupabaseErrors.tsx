5921a5727c7a0eaeb5416bf4d4a0ea80d827a1c2
gcoogan1 <gcoogan1@gmail.com>
2026-01-22 11:35:42 -0500

    refactor: update handeSupabasseError function to use code instead of status

--- src/utils/handleSupabaseErrors.tsx
+++ src/utils/handleSupabaseErrors.tsx
@@ -3,30 +3,19 @@ import { modalVariants } from "@/types/modal.types";
 
 // -- Supabase Modal Error Handler -- //
 
-// Used to handle general supabase errors and open appropriate modals
-
 type SupabaseError = {
-  status?: number;
+  code?: keyof typeof modalVariants;
 };
 
 export const handleSupabaseError = (
   error: SupabaseError,
   openModal: ReturnType<typeof useModal>["openModal"]
 ) => {
-  // Map of supabase status codes to modal types
-  const statusModalMap: Record<number, keyof typeof modalVariants> = {
-    409: "EXISTING_EMAIL",
-    422: "EXISTING_ACCOUNT",
-    429: "ATTEMPT_MAX",        
-    430: "REQUEST_MAX", 
-    500: "SERVER_ERROR",       
-  };
+  const modalKey =
+    error.code && modalVariants[error.code]
+      ? error.code
+      : "SERVER_ERROR";
 
-  const modalKey = error.status && statusModalMap[error.status]
-    ? statusModalMap[error.status]
-    : "SERVER_ERROR";      
-
-  // render the appropriate modal
   const ModalComponent = modalVariants[modalKey];
   openModal(<ModalComponent />);
 };
