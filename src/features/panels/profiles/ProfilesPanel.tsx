aba57af5e969b20c9dffd0cc8a8278efa57248c7
gcoogan1 <gcoogan1@gmail.com>
2026-01-28 14:25:51 -0500

    feat: create empty profile panel

--- src/features/panels/profiles/ProfilesPanel.tsx
+++ src/features/panels/profiles/ProfilesPanel.tsx
@@ -1,19 +1,10 @@
-import EmptyMessage from "@/components/Messages/EmptyMessage/EmptyMessage";
-import PanelLayout from "@/components/Panels/components/PanelLayout/PanelLayout";
-import Profile from "@assets/Icon/Profile.svg?react";
+import EmptyProfilesPanel from "./EmptyProfilePanel";
+
+//TODO: Add logic to show profiles when they exist
 
 const ProfilesPanel = () => {
   return (
-    <PanelLayout 
-      panelTitle="Profiles" 
-      panelTitleIcon={<Profile />}
-      >
-      <EmptyMessage
-        title="No Profiles Created"
-        icon={<Profile />}
-        subtitle="Start participating in Leagues by creating your first driver’s Profile."
-      />
-    </PanelLayout>
+    <EmptyProfilesPanel />
   );
 };
 
