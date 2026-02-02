172cdf4e299794a7cc18a5a79f46febbfe5ed156
gcoogan1 <gcoogan1@gmail.com>
2026-02-02 15:56:53 -0500

    feat: create profile header component

---
+++ src/components/Headers/ProfileHeader/ProfileHeader.styles.ts
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+import { designTokens } from "@app/design/tokens";
+
+
+const { colors, gradients, layout, borders, typography } = designTokens;
+
+export const ProfileHeaderContainer = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  width: 100%;
+  /* min-width: 920px; */
+  min-height: 316px;
+
+  ${layout.mediaQueries.mobile} {
+    min-width: 360px;
+    max-width: 920px;
+    min-height: 440px;
+    justify-content: flex-end;
+    flex-direction: column;
+  }
+`
+
+
+export const Frame = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 240px;
+  top: 0;
+  border-bottom-left-radius: ${borders.radius.round};
+  background: ${gradients.base.fadeRight10};
+
+  ${layout.mediaQueries.mobile} {
+    height: 160px;
+  }
+`;
+
+export const Contents = styled.div`
+  width: 100%;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-self: flex-end;
+  padding: 0 ${layout.space.xxLarge};
+
+  ${layout.mediaQueries.mobile} {
+    padding: 0 ${layout.space.xLarge};
+  }
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: ${layout.space.medium};
+
+  ${layout.mediaQueries.mobile} {
+    gap: 0;
+    position: relative;
+  }
+`;
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  border-radius: ${borders.radius.round};
+  background: ${colors.base.base2};
+  padding: ${layout.space.small};
+`;
+
+export const Details = styled.div`
+  display: flex;
+  flex: 1;
+  gap: ${layout.space.xLarge};
+  min-height: 200px;
+
+  ${layout.mediaQueries.mobile} {
+    flex-direction: column;
+    align-items: center;
+    gap: ${layout.space.medium};
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex: 1;
+  padding-top: 20px;
+  flex-direction: column;
+  gap: 48px;
+
+  ${layout.mediaQueries.mobile} {
+    align-items: center;
+    gap: ${layout.space.medium};
+  }
+`;
+
+export const TextContent = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: ${layout.space.xSmall};
+
+  ${layout.mediaQueries.mobile} {
+    align-items: center;
+    gap: ${layout.space.xxxSmall};
+  }
+`;
+
+export const Username = styled.h2`
+  ${typography.title.large}
+  color: ${colors.text.text1};
+  text-transform: capitalize;
+`;
+
+export const UserGame = styled.p`
+  ${typography.subtitle.xLargeItalic}
+  color: ${colors.text.text2};
+`;
+
+export const TagContent = styled.div`
+  display: flex;
+  gap: ${layout.space.small};
+  align-items: center;
+
+  ${layout.mediaQueries.mobile} {
+    flex-direction: column;
+  }
+`;
+
+export const ChampionCountContent = styled.div`
+  display: flex;
+  border-radius: ${borders.radius.round};
+  justify-content: center;
+  align-items: center;;
+  padding: ${layout.space.xxSmall} ${layout.space.small};
+  background: ${colors.role.champion};
+`;
+
+export const ChampionCount = styled.p`
+  ${typography.body.mediumBold}
+  color: ${colors.text.text1};
+`;
+
+export const Actions = styled.div` 
+  display: flex;
+  align-self: flex-start;
+  gap: ${layout.space.xSmall};
+  padding-top: 56px;
+
+    ${layout.mediaQueries.mobile} {
+    position: absolute;
+    right: 0;
+  }
+`
---
+++ src/components/Headers/ProfileHeader/ProfileHeader.tsx
@@ -0,0 +1,96 @@
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import {
+  Actions,
+  AvatarContainer,
+  ChampionCount,
+  ChampionCountContent,
+  Contents,
+  Details,
+  DetailsContainer,
+  Frame,
+  ProfileHeaderContainer,
+  TagContent,
+  TextContainer,
+  TextContent,
+  UserGame,
+  Username,
+} from "./ProfileHeader.styles";
+import Button from "@/components/Button/Button";
+import EditIcon from "@assets/Icon//Edit.svg?react";
+import FollowersIcon from "@assets/Icon/Followers.svg?react";
+import Avatar from "@/components/Avatar/Avatar";
+import type { AvatarVariants } from "@/components/Avatar/Avatar.variants";
+
+type ProfileHeaderProps = {
+  gameType: string;
+  username: string;
+  editInClick: () => void;
+  avatarType?: AvatarVariants;
+  imageUrl?: string;
+  followersOnClick?: () => void;
+  followersCount?: number;
+  championCount?: number;
+};
+
+const ProfileHeader = ({
+  gameType,
+  username,
+  editInClick,
+  avatarType = "none",
+  imageUrl,
+  followersOnClick,
+  followersCount,
+  championCount,
+}: ProfileHeaderProps) => {
+  const isMobile = useMediaQuery("(max-width: 919px)");
+
+  return (
+    <ProfileHeaderContainer>
+      <Frame />
+      <Contents>
+        <DetailsContainer>
+          <Details>
+            <AvatarContainer>
+              <Avatar size="xxLarge" type={avatarType} imageUrl={imageUrl || undefined} />
+            </AvatarContainer>
+            <TextContainer>
+              <TextContent>
+                <Username>{username}</Username>
+                <UserGame>{gameType}</UserGame>
+              </TextContent>
+              <TagContent>
+                {!!followersCount && (
+                  <Button
+                    color="base"
+                    icon={{ left: <FollowersIcon /> }}
+                    onClick={followersOnClick}
+                    rounded
+                  >
+                    {followersCount} Followers
+                  </Button>
+                )}
+                {!!championCount && (
+                  <ChampionCount>
+                    <ChampionCountContent>{championCount}x Champion</ChampionCountContent>
+                  </ChampionCount>
+                )}
+              </TagContent>
+            </TextContainer>
+          </Details>
+          <Actions>
+            <Button
+              color="base"
+              icon={{ left: <EditIcon /> }}
+              onClick={editInClick}
+              fullWidth
+            >
+              {isMobile ? null : "Edit Profile"}
+            </Button>
+          </Actions>
+        </DetailsContainer>
+      </Contents>
+    </ProfileHeaderContainer>
+  );
+};
+
+export default ProfileHeader;
