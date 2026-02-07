3e4cacc98886503d5c5e2063a59e87722a45e7ea
gcoogan1 <gcoogan1@gmail.com>
2026-02-07 11:49:25 -0500

    reafcator: move auth check and fetchs into app

--- src/pages/Hompage/Homepage.tsx
+++ src/pages/Hompage/Homepage.tsx
@@ -1,28 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { useAppTheme } from "@/providers/theme/useTheme";
 import { LogoThemes } from "@/app/design/logoThemes";
 import { Container, SubTitle, Wrapper } from "./Homepage.styles";
-import { useAuth } from "@/providers/auth/useAuth";
-import type { AppDispatch } from "@/store";
-import { fetchAccountThunk } from "@/store/account/account.thunks";
-import { fetchProfilesThunk } from "@/store/profile/profile.thunk";
 
 const Homepage = () => {
   // Theme
   const { themeName } = useAppTheme();
   const LogoIcon = LogoThemes[themeName];
-  
-  // Auth & Account
-  const { user } = useAuth();
-  const dispatch = useDispatch<AppDispatch>();
-
-  useEffect(() => {
-    if (user?.id) {
-      dispatch(fetchAccountThunk(user.id));
-      dispatch(fetchProfilesThunk(user.id));
-    }
-  }, [user?.id, dispatch]);
 
   return (
     <Wrapper>
