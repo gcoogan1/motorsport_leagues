2744cc6afb0948181e0f5f7583100070e3667cd8
gcoogan1 <gcoogan1@gmail.com>
2026-03-01 18:06:17 -0500

    feat: add logic to display my squads on profile page

--- src/components/Cards/CardList/SquadsListCard/SquadsListCard.tsx
+++ src/components/Cards/CardList/SquadsListCard/SquadsListCard.tsx
@@ -2,6 +2,7 @@ import EmptyMessage from "@/components/Messages/EmptyMessage/EmptyMessage";
 import SquadIcon from "@assets/Icon/Squad.svg?react";
 import CreateIcon from "@assets/Icon/Create.svg?react";
 import SearchIcon from "@assets/Icon/Search.svg?react";
+import SquadCard from "../../SquadCard/SquadCard";
 import {
   CardListWrapper,
   HeaderContainer,
@@ -9,30 +10,60 @@ import {
   Title,
 } from "./SquadsListCard.styles";
 
-const SquadsListCard = () => {
+type SquadsListCardProps = {
+  squads: Array<{
+    id: string;
+    name: string;
+    bannerUrl?: string;
+    membersCount?: number;
+    onClick: () => void;
+  }>;
+  isOwner: boolean;
+};  
+
+const SquadsListCard = ({ squads, isOwner }: SquadsListCardProps) => {
   return (
     <CardListWrapper>
       <HeaderContainer>
-        <Title>List of Squads</Title>
+        <Title>My Squads</Title>
       </HeaderContainer>
       <ListContainer>
-        <EmptyMessage
-          title="No Squads Created or Joined"
-          icon={<SquadIcon />}
-          subtitle="Start building your racing community by creating or joining your first Squad with a Profile."
-          actions={{
-            primary: {
-              label: "Create New Squad",
-              onClick: () => {},
-              leftIcon: <CreateIcon />,
-            },
-            secondary: {
-              label: "Find a Squad",
-              onClick: () => {},
-              leftIcon: <SearchIcon />,
-            },
-          }}
-        />
+        {squads && squads.length > 0 ? (
+          squads.map((squad) => (
+            <SquadCard
+              key={squad.id}
+              name={squad.name}
+              size="medium"
+              bannerImageUrl={squad.bannerUrl}
+              memberCount={squad.membersCount ?? 0}
+              onClick={squad.onClick}
+            />
+          ))
+        ) : !isOwner ? (
+          <EmptyMessage
+            title="No Squads Created or Joined"
+            subtitle="This Profile is currently not part of any Squads."
+            icon={<SquadIcon />}
+          />
+        ) : (
+          <EmptyMessage
+            title="No Squads Created or Joined"
+            icon={<SquadIcon />}
+            subtitle="Start building your racing community by creating or joining your first Squad with a Profile."
+            actions={{
+              primary: {
+                label: "Create New Squad",
+                onClick: () => {},
+                leftIcon: <CreateIcon />,
+              },
+              secondary: {
+                label: "Find a Squad",
+                onClick: () => {},
+                leftIcon: <SearchIcon />,
+              },
+            }}
+          />
+        )}
       </ListContainer>
     </CardListWrapper>
   );
