2d1df1ab2921ee88e178fa65950320180246a484
gcoogan1 <gcoogan1@gmail.com>
2026-03-09 10:47:28 -0400

    feat: create chip component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,44 @@
+import Avatar from "../Avatar/Avatar";
+import CloseIcon from "@assets/Icon/Close.svg?react";
+import { ChipWrapper, ProfileTextContainer, Username, Game, ChipButton, TagText, ProfileInfoContainer } from "./Chip.styles";
+
+type ChipProps = {
+  type: "profile" | "tag"
+  onClick?: () => void;
+  profile?: {
+    avatarType: "preset" | "upload";
+    avatarValue: string;
+    username: string;
+    game: string;
+  }
+  tagText?: string;
+}
+
+const Chip = ({ type, onClick, profile, tagText }: ChipProps) => {
+  return (
+    <ChipWrapper type={type}>
+      {type === "profile" && profile && (
+        <>
+        
+        <ProfileInfoContainer>
+          <Avatar avatarType={profile.avatarType} avatarValue={profile.avatarValue} size="small" />
+          <ProfileTextContainer>
+            <Username>{profile.username}</Username>
+            <Game>{profile.game}</Game>
+          </ProfileTextContainer>
+        </ProfileInfoContainer>
+          <ChipButton onClick={onClick} aria-label={`View ${profile.username}'s profile`}>
+            <CloseIcon width={18} height={18} />
+          </ChipButton>
+        </>
+      )}
+      {type === "tag" && tagText && (
+        <>
+          <TagText>{tagText}</TagText>
+        </>
+      )}
+    </ChipWrapper>
+  )
+}
+
+export default Chip
