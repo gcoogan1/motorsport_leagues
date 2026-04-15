10532bb8123d0fc6e27b04e44692f42b2d7361ee
gcoogan1 <gcoogan1@gmail.com>
2026-04-15 13:03:50 -0400

    feat: update game icon asset; update status to use league status type; create league card

---
+++ src/components/Cards/LeagueCard/LeagueCard.styles.ts
@@ -0,0 +1,158 @@
+import styled, { css } from "styled-components";
+import { designTokens } from "@/app/design/tokens";
+import { gradientBorder } from "@/app/design/mixens/gradientBorder";
+
+const { colors, layout, borders, typography } = designTokens;
+
+
+export const ClickableWrapper = styled.button<{ $cardSize: "small" | "medium" }>`
+  /* Reset Styles */
+  appearance: none;
+  border: none;
+  background: none;
+  padding: 0;
+  font: inherit;
+  color: inherit;
+  z-index: 0;
+  overflow: hidden;
+  cursor: pointer;
+
+  width: 100%;
+  /* min-width: 264px; */
+  max-width: 384px;
+  border-bottom-left-radius: ${borders.radius.xxLarge};
+  border-bottom-right-radius: ${borders.radius.xxLarge};
+  background: ${({ theme }) => theme.theme.primaryGradientFadeTop10};
+  padding: ${layout.space.small};
+  gap: ${layout.space.small};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  ${({ theme }) => gradientBorder({ gradient: theme.theme.primaryGradientFadeTop10, width: borders.width.medium })};
+
+  /* Interactive States */
+  &:hover {
+    border-radius: ${borders.radius.xxLarge};
+    ${({ theme }) => gradientBorder({ gradient: theme.theme.primary30, width: borders.width.medium })};
+  }
+
+  &:focus-visible {
+    border-radius: ${borders.radius.xxLarge};
+    ${({ theme }) => gradientBorder({ gradient: theme.theme.primary30, width: borders.width.medium })};
+    outline: 2px solid ${colors.utility.focus};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    border-radius: ${borders.radius.xxLarge};
+    background: ${colors.base.translucent10};
+    ${({ theme }) => gradientBorder({ gradient: theme.theme.primaryA, width: borders.width.medium })};
+  } 
+
+  /* Small Variant Overrides (Targeting Children) */
+  ${({ $cardSize }) => $cardSize === "small" && css`
+    border-radius: ${borders.radius.medium};
+    flex-direction: row;
+  `}
+`;
+
+
+export const ImageContainer = styled.div<{ $cardSize: "small" | "medium", $imageBg?: string }>`
+  width: 100%;
+  height: 160px;
+  align-self: stretch;
+  border-radius: ${borders.radius.medium};
+  background-color: ${colors.base.translucent10};
+  background-image: ${({ $imageBg }) => $imageBg ? `url(${$imageBg})` : "none"};
+  background-size: cover;
+  background-position: center;
+  flex-shrink: 0;
+
+  /* Small Variant Overrides */
+  ${({ $cardSize }) => $cardSize === "small" && css`
+    width: 180px;
+    height: 80px;
+    align-self: center;
+    border-radius: ${borders.radius.small};
+  `}
+`;
+
+export const IndicatorsContainer = styled.div`
+  display: flex;
+  padding: ${layout.space.xSmall};
+  justify-content: flex-end;
+  border-radius: inherit;
+  align-items: center;
+  gap: ${layout.space.xxSmall};
+  align-self: stretch;
+  background: ${({ theme }) => theme.theme.primaryGradientFadeLeft};
+  flex-wrap: wrap;
+`
+
+export const TextContainer = styled.div<{ $cardSize: "small" | "medium" }>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px ${layout.space.small};
+  gap: ${layout.space.xxSmall};
+  text-align: left;
+  align-self: baseline;
+  flex: 1;
+  width: 100%;
+  
+  ${({ $cardSize }) => $cardSize === "small" && css`
+    display: flex;
+    min-width: 80px;
+    padding: 0px;
+    flex: 1 0 0;
+  `}
+`
+
+export const LeagueName = styled.h3`
+  ${typography.title.xSmall};
+  color: ${colors.text.text1};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+`
+
+/* Details Section for Medium Size */
+export const LeagueInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${layout.space.xSmall};
+  align-self: stretch;
+  color: ${colors.text.text2};
+`
+
+export const LeagueInfoContent = styled.div`
+  display: flex;
+  gap: ${layout.space.xxSmall};
+`
+
+export const LeagueInfoText = styled.p`
+  ${typography.body.smallBold};
+  color: ${colors.text.text2};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 24px;
+  background: ${colors.base.translucent10};
+`
+
+
+/* Squad Info for Small Size */
+export const SquadInfoText = styled.p`
+  ${typography.body.smallRegular};
+  color: ${colors.text.text2};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
---
+++ src/components/Cards/LeagueCard/LeagueCard.tsx
@@ -0,0 +1,89 @@
+import type { Tag } from "@/components/Tags/Tags.variants";
+import type { LeagueStatus } from "@/types/league.types";
+import type { GameType } from "@/types/profile.types";
+import Icon from "@/components/Icon/Icon";
+import ParticipantsIcon from "@/assets/Icon/participants.svg?react";
+import GameIcon from "@/assets/Icon/game.svg?react";
+import HostIcon from "@/assets/Icon/hosts.svg?react";
+import { ClickableWrapper, Divider, ImageContainer, IndicatorsContainer, LeagueInfoContainer, LeagueInfoContent, LeagueInfoText, LeagueName, SquadInfoText, TextContainer } from "./LeagueCard.styles";
+import Status from "@/components/Status/Status";
+import Tags from "@/components/Tags/Tags";
+
+type LeagueCardProps = {
+  name: string;
+  coverImageUrl: string;
+  seasonStatus: LeagueStatus;
+  size: "small" | "medium";
+  hostingSquad?: string;
+  numOfParticipants?: number;
+  gameType?: GameType;
+  tags?: Tag[];
+  onClick?: () => void;
+}
+
+const LeagueCard = ({
+  name,
+  coverImageUrl,
+  seasonStatus,
+  size,
+  hostingSquad,
+  numOfParticipants,
+  gameType,
+  tags,
+  onClick
+}: LeagueCardProps) => {
+  const visibleTags = tags?.slice(0, size === "medium" ? 3 : 2);
+
+  const gameTypeMap: Record<GameType, string> = {
+    gt7: "GT7",
+    iRacing: "iRacing",
+    assetoCorsaEvo: "Evo",
+    leMansUltimate: "LMU",
+  }
+
+  return (
+    <ClickableWrapper onClick={onClick} $cardSize={size}>
+      <ImageContainer $cardSize={size} $imageBg={coverImageUrl}>
+        <IndicatorsContainer>
+          {visibleTags && <Tags variants={visibleTags} />}
+          {seasonStatus && <Status statusType={seasonStatus} />}
+        </IndicatorsContainer>
+      </ImageContainer>
+      <TextContainer $cardSize={size}>
+        <LeagueName>{name}</LeagueName>
+        {size === "medium" ? (
+          <LeagueInfoContainer>
+              {numOfParticipants && (
+                <>
+                <LeagueInfoContent>
+                  <Icon size="small"><ParticipantsIcon /></Icon>
+                  <LeagueInfoText>{numOfParticipants}</LeagueInfoText>
+                </LeagueInfoContent>
+                <Divider />
+                </>
+              )}
+              {gameType && (
+                <>
+                  <LeagueInfoContent>
+                    <Icon size="small"><GameIcon /></Icon>
+                    <LeagueInfoText>{gameTypeMap[gameType]}</LeagueInfoText>
+                  </LeagueInfoContent>
+                  <Divider />
+                </>
+              )}
+              {hostingSquad && (
+                <LeagueInfoContent>
+                  <Icon size="small"><HostIcon /></Icon>
+                  <LeagueInfoText>{hostingSquad}</LeagueInfoText>
+                </LeagueInfoContent>
+              )}
+          </LeagueInfoContainer>
+        ) : (
+          <SquadInfoText>{`Hosted by ${hostingSquad}`}</SquadInfoText>
+        )}
+      </TextContainer>
+    </ClickableWrapper>
+  )
+}
+
+export default LeagueCard
