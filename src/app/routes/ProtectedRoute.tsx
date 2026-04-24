15f5e33fa11613732343c4b212bf48d250326969
gcoogan1 <gcoogan1@gmail.com>
2026-04-24 10:06:38 -0400

    fix: update protected route logout to go to login page

--- src/app/routes/ProtectedRoute.tsx
+++ src/app/routes/ProtectedRoute.tsx
@@ -5,12 +5,16 @@ import { useAuth } from "@/providers/auth/useAuth";
 // TODO: Add loading state while auth is being determined (if using async auth check)
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
 
+  if (loading) {
+    return null;
+  }
+
   if (!user) {
-    // Redirect to an unavailable page
-    return <Navigate to="/unavailable" state={{ from: location }} replace />;
+    // Redirect to login page if user is not authenticated
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return <>{children}</>;
