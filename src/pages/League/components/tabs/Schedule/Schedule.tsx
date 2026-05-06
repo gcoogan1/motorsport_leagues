09763e016fdd092063e961e5544465dfd7c08af8
gcoogan1 <gcoogan1@gmail.com>
2026-05-06 14:52:01 -0400

    feat: create leage tab components

---
+++ src/pages/League/components/tabs/Schedule/Schedule.tsx
@@ -0,0 +1,29 @@
+import SetupIcon from "@assets/Icon/Season_Setup.svg?react";
+import EmptyMessage from "@/components/Messages/EmptyMessage/EmptyMessage";
+import type { LeagueStatus } from "@/types/league.types";
+import ContentTextBlock from "@/components/Structures/ContentBlocks/ContentTextBlock/ContentTextBlock";
+
+type ScheduleProps = {
+  seasonStatus: LeagueStatus;
+};
+
+const Schedule = ({ seasonStatus }: ScheduleProps) => {
+  return (
+    <>
+      {seasonStatus === "setup" ? (
+        <EmptyMessage
+          icon={<SetupIcon />}
+          title="Coming Soon"
+          subtitle="The latest Season of this League is being set up!"
+        />
+      ) : (
+        <ContentTextBlock
+          title="Schedule"
+          message="Enter some text here to provide description about this season. This content blocks in the overview tab are unique for each season, along with the poster image."
+        />
+      )}
+    </>
+  );
+};
+
+export default Schedule;
