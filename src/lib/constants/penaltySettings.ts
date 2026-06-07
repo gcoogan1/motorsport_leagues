46d65245e320a3c2d2ec1ae91f31dc70966d3fca
gcoogan1 <gcoogan1@gmail.com>
2026-06-06 23:25:23 -0400

    feat: add penalty settings data and modal

---
+++ src/lib/constants/penaltySettings.ts
@@ -0,0 +1,37 @@
+export const SHORTCUT_PENALTY_OPTIONS = [
+  {  label: "Off", value: "off" },
+  { label: "Weak", value: "weak" },
+  { label: "Strong", value: "strong" },
+]
+
+export const WALL_COLLISION_PENALTY_OPTIONS = [
+  { label: "Off", value: "off" },
+  { label: "Weak", value: "weak" },
+  { label: "Strong", value: "strong" },
+];
+
+export const CORRECT_VEHICLE_COURSE_OPTIONS = [
+  { label: "Off", value: "false" },
+  { label: "On", value: "true" },
+]
+
+export const CAR_COLLISION_PENALTY_OPTIONS = [
+  { label: "Off", value: "false" },
+  { label: "On", value: "true" },
+];
+
+export const PIT_LANE_LINE_CUT_OPTIONS = [
+  { label: "Off", value: "false" },
+  { label: "On", value: "true" },
+];
+
+export const GHOSTING_DURING_RACE_OPTIONS = [
+  { label: "Off", value: "off" },
+  { label: "Weak", value: "weak" },
+  { label: "Strong", value: "strong" },
+];
+
+export const FLAG_RULES_OPTIONS = [
+  { label: "Off", value: "false" },
+  { label: "On", value: "true" },
+];
